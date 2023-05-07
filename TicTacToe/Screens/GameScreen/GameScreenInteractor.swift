78edd9e9a9d0c93a83afa0d7764c3370048823df
Gerasimchuk Elena <zziikk2003@gmail.com>
2023-05-07 22:24:59 +0700

    Add game over logic

--- TicTacToe/Screens/GameScreen/GameScreenInteractor.swift
+++ TicTacToe/Screens/GameScreen/GameScreenInteractor.swift
@@ -5,48 +5,44 @@
 //  Created by Елена on 07.05.2023.
 //
 
+import Foundation
+
 final class GameScreenInteractor {
     weak var presenter: GameScreenPresenterProtocol?
+    var businessLogic: GameBusinessLogic
     
     var nameFirstPlayer: String
     var nameSecondPlayer: String
-    var nowTurnPlayerShape: PlayerShapeType
-    var gameMatrix = [[PlayerShapeType]]()
     
     init() {
+        businessLogic = GameBusinessLogic()
         nameFirstPlayer = "Nana"
         nameSecondPlayer = "Bla"
-        nowTurnPlayerShape = .cross
-        
-        for _ in 0..<9 {
-            var array = [PlayerShapeType]()
-            for _ in 0..<9 {
-                array.append(PlayerShapeType.non)
-            }
-            
-            gameMatrix.append(array)
-        }
     }
 }
 
 extension GameScreenInteractor: GameScreenInteractorProtocol {
     func saveNewShape(row: Int, column: Int) {
-        gameMatrix[row][column] = nowTurnPlayerShape
+        businessLogic.saveNewShape(row: row, column: column)
     }
     
     func moveToNextPlayer() {
-        nowTurnPlayerShape = nowTurnPlayerShape == .cross ? .circle : .cross
+        businessLogic.changeCurrentPlayer()
     }
     
     func checkPossibleMoveInGame(row: Int, column: Int) -> Bool {
-        gameMatrix[row][column] == .non ? true : false
+        businessLogic.checkPossibleMoveInGame(row: row, column: column)
     }
     
     func getCurrentPlayerShape() -> PlayerShapeType {
-        nowTurnPlayerShape
+        businessLogic.getCurrentPlayerShape()
     }
     
     func getNameCurrentPlayer() -> String {
-        nowTurnPlayerShape == .cross ? nameFirstPlayer : nameSecondPlayer
+        businessLogic.getCurrentPlayerShape() == .cross ? nameFirstPlayer : nameSecondPlayer
+    }
+    
+    func checkIfGameOver() -> Bool {
+        businessLogic.checkPresenseNonShape()
     }
 }
---
+++ TicTacToe/Services/GameBusinessLogic.swift
@@ -0,0 +1,147 @@
+//
+//  GameBusinessLogic.swift
+//  TicTacToe
+//
+//  Created by Елена on 07.05.2023.
+//
+
+import Foundation
+
+class GameBusinessLogic {
+    var gameMatrix = [[PlayerShapeType]]()
+    var nowTurnPlayerShape: PlayerShapeType
+    
+    init() {
+        nowTurnPlayerShape = .cross
+        
+        for _ in 0..<3 {
+            var array = [PlayerShapeType]()
+            for _ in 0..<3 {
+                array.append(PlayerShapeType.non)
+            }
+            
+            gameMatrix.append(array)
+        }
+    }
+    
+    private func checkPresenseWinnerPositionOnByRow() -> Bool {
+        for i in 0..<gameMatrix.count {
+            if gameMatrix[i][0] == .non {
+                continue
+            }
+            
+            var isWin = true
+            for j in 0..<gameMatrix.count {
+                if gameMatrix[i][j] != gameMatrix[i][0] {
+                    isWin = false
+                    break
+                }
+            }
+            
+            if isWin {
+                return true
+            }
+        }
+        
+        return false
+    }
+    
+    private func checkPresenseWinnerPositionOnByColumn() -> Bool {
+        for i in 0..<gameMatrix.count {
+            if gameMatrix[0][i] == .non {
+                continue
+            }
+        
+            var isWin = true
+            for j in 0..<gameMatrix.count {
+                if gameMatrix[j][i] != gameMatrix[0][i] {
+                    isWin = false
+                    break
+                }
+            }
+            
+            if isWin {
+                return true
+            }
+        }
+        
+        return false
+    }
+    
+    private func checkPresenseWinnerPositionOnDiagonals() -> Bool {
+        var isWin = true
+        
+        if gameMatrix[0][0] == .non {
+            return false
+        }
+        
+        for i in 0..<gameMatrix.count {
+            if gameMatrix[i][i] != gameMatrix[0][0] {
+                isWin = false
+                break
+            }
+        }
+        
+        if isWin {
+            return true
+        }
+        
+        if gameMatrix[0][gameMatrix.count - 1] == .non {
+            return false
+        }
+        
+        for i in 0..<gameMatrix.count {
+            if gameMatrix[i][gameMatrix.count - 1 - i] != gameMatrix[0][gameMatrix.count - 1] {
+                isWin = false
+                break
+            }
+        }
+        
+        if isWin {
+            return true
+        }
+        
+        return false
+    }
+    
+    func saveNewShape(row: Int, column: Int) {
+        gameMatrix[row - 1][column - 1] = nowTurnPlayerShape
+        let _ = checkPresenseWinnerPosition()
+    }
+    
+    func checkPossibleMoveInGame(row: Int, column: Int) -> Bool {
+        gameMatrix[row - 1][column - 1] == .non ? true : false
+    }
+    
+    func checkPresenseNonShape() -> Bool {
+        for row in gameMatrix {
+            for cell in row {
+                if cell == .non {
+                    return false
+                }
+            }
+        }
+        
+        return true
+    }
+    
+    func checkPresenseWinnerPosition() -> Bool {
+        if checkPresenseWinnerPositionOnByRow() ||
+            checkPresenseWinnerPositionOnByColumn() ||
+            checkPresenseWinnerPositionOnDiagonals() {
+            
+            print("Game over")
+            return true
+        }
+        
+        return false
+    }
+    
+    func getCurrentPlayerShape() -> PlayerShapeType {
+        nowTurnPlayerShape
+    }
+    
+    func changeCurrentPlayer() {
+        nowTurnPlayerShape = nowTurnPlayerShape == .cross ? .circle : .cross
+    }
+}
