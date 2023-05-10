a8ae8388277d4108deb58083e580d2edb7ebf6d3
Gerasimchuk Elena <zziikk2003@gmail.com>
2023-05-10 14:15:43 +0700

    Add description for error

---
+++ TicTacToe/UseCases/Constants/DescriptionError.swift
@@ -0,0 +1,11 @@
+//
+//  DescriptionError.swift
+//  TicTacToe
+//
+//  Created by Елена on 10.05.2023.
+//
+
+struct DescriptionError {
+    static let emptyField = "Заполните все поля с именами игроков и повторите попытку"
+    static let notValidNamePlayer = "Имена игроков должны состоять только из букв английского или русского алфавита"
+}
